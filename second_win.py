1c0fa7c01471bca4460ffb0e4ddcc6e4976ffa26
amzi11 <sukonobines@gmail.com>
2025-08-04 21:28:14 +0300

    update list2

--- second_win.py
+++ second_win.py
@@ -1,6 +1,7 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, Qtime
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit
 from instr import *
+
 
 class SecondWin(QWidget):
     def __init__(self):
@@ -15,7 +16,7 @@ class SecondWin(QWidget):
         self.lbl_name = QLabel(txt_name)
         self.inpt_name = QLineEdit()
         self.inpt_name.setPlaceholderText(txt_hintname)
-        self.lbl_age = QLabel(txt_name)
+        self.lbl_age = QLabel(txt_age)
         self.inpt_age = QLineEdit()
         self.inpt_age.setPlaceholderText(txt_hintage)
 
@@ -25,7 +26,52 @@ class SecondWin(QWidget):
         self.inpt_pulse1.setPlaceholderText(txt_hinttest1)
         self.inpt_pulse1.setDisabled(True)
 
+        self.lbl_instr2 = QLabel(txt_test2)
+        self.btn_instr2 = QPushButton(txt_starttest2)
+        self.btn_instr2.setDisabled(True)
 
+        self.lbl_test2 = QLabel(txt_test3)
+        self.btn_test2 = QPushButton(txt_starttest3)
+        self.btn_test2.setDisabled(True)
+        self.inpt_pulse2 = QLineEdit()
+        self.inpt_pulse2.setPlaceholderText(txt_hintage)
+        self.inpt_pulse2.setDisabled(True)
+        self.inpt_pulse3 = QLineEdit()
+        self.inpt_pulse3.setPlaceholderText(txt_hintage)
+        self.inpt_pulse3.setDisabled(True)
+        self.btn_result = QPushButton(txt_sendresults)
+        self.btn_result.setDisabled(True)
+
+
+        self.lbl_timer = QLabel(txt_timer)
+
+        self.layout_v1 = QVBoxLayout()
+
+        self.layout_v1.addWidget(self.lbl_name, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.inpt_name, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.lbl_age, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.inpt_age, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.lbl_test1, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.btn_test1, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.inpt_pulse1, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.lbl_instr2, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.btn_instr2, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.lbl_test2, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.btn_test2, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.inpt_pulse2, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.inpt_pulse3, alignment = Qt.AlignLeft)
+        self.layout_v1.addWidget(self.btn_result, alignment = Qt.AlignCenter)
+        
+        
+        self.layout_v2 = QVBoxLayout()
+        self.layout_v2.addWidget(self.lbl_timer, alignment = Qt.AlignRight)
+
+        self.layout_main = QHBoxLayout()
+        self.layout_main.addLayout(self.layout_v1)
+        self.layout_main.addLayout(self.layout_v2)
+
+        self.setLayout(self.layout_main)
+                 
     def next_click(self):
         pass
 
